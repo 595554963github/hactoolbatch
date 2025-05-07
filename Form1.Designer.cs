1bfed27e26190a725f1f4cf938e79c8f34a555bd
595554963github <595554963@qq.com>
2025-05-08 00:45:13 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace HactoolUnpacker
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.btnSelectXci = new System.Windows.Forms.Button();
+            this.btnUnpackXCI = new System.Windows.Forms.Button();
+            this.btnSelectNca = new System.Windows.Forms.Button();
+            this.btnUnpackNCA = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbOutput.Location = new System.Drawing.Point(12, 12);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(560, 300);
+            this.rtbOutput.TabIndex = 0;
+            this.rtbOutput.Text = "";
+            // 
+            // btnSelectXci
+            // 
+            this.btnSelectXci.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSelectXci.Location = new System.Drawing.Point(12, 318);
+            this.btnSelectXci.Name = "btnSelectXci";
+            this.btnSelectXci.Size = new System.Drawing.Size(120, 30);
+            this.btnSelectXci.TabIndex = 1;
+            this.btnSelectXci.Text = "选择XCI文件";
+            this.btnSelectXci.UseVisualStyleBackColor = true;
+            this.btnSelectXci.Click += new System.EventHandler(this.btnSelectXci_Click);
+            // 
+            // btnUnpackXCI
+            // 
+            this.btnUnpackXCI.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnUnpackXCI.Location = new System.Drawing.Point(138, 318);
+            this.btnUnpackXCI.Name = "btnUnpackXCI";
+            this.btnUnpackXCI.Size = new System.Drawing.Size(120, 30);
+            this.btnUnpackXCI.TabIndex = 2;
+            this.btnUnpackXCI.Text = "解包XCI";
+            this.btnUnpackXCI.UseVisualStyleBackColor = true;
+            this.btnUnpackXCI.Click += new System.EventHandler(this.btnUnpackXCI_Click);
+            // 
+            // btnSelectNca
+            // 
+            this.btnSelectNca.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSelectNca.Location = new System.Drawing.Point(264, 318);
+            this.btnSelectNca.Name = "btnSelectNca";
+            this.btnSelectNca.Size = new System.Drawing.Size(120, 30);
+            this.btnSelectNca.TabIndex = 3;
+            this.btnSelectNca.Text = "选择NCA文件";
+            this.btnSelectNca.UseVisualStyleBackColor = true;
+            this.btnSelectNca.Click += new System.EventHandler(this.btnSelectNca_Click);
+            // 
+            // btnUnpackNCA
+            // 
+            this.btnUnpackNCA.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnUnpackNCA.Location = new System.Drawing.Point(390, 318);
+            this.btnUnpackNCA.Name = "btnUnpackNCA";
+            this.btnUnpackNCA.Size = new System.Drawing.Size(120, 30);
+            this.btnUnpackNCA.TabIndex = 4;
+            this.btnUnpackNCA.Text = "解包NCA";
+            this.btnUnpackNCA.UseVisualStyleBackColor = true;
+            this.btnUnpackNCA.Click += new System.EventHandler(this.btnUnpackNCA_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.btnUnpackNCA);
+            this.Controls.Add(this.btnSelectNca);
+            this.Controls.Add(this.btnUnpackXCI);
+            this.Controls.Add(this.btnSelectXci);
+            this.Controls.Add(this.rtbOutput);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "Form1";
+            this.Text = "Hactool解包工具";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Button btnSelectXci;
+        private System.Windows.Forms.Button btnUnpackXCI;
+        private System.Windows.Forms.Button btnSelectNca;
+        private System.Windows.Forms.Button btnUnpackNCA;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace HactoolUnpacker
+{
+    public partial class Form1 : Form
+    {
+        private string selectedXciFilePath = "";
+        private string selectedNcaFilePath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectXciFile()
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                dialog.Title = "选择XCI文件";
+                dialog.Filter = "XCI文件 (*.xci)|*.xci";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedXciFilePath = dialog.FileName;
+                    UpdateRichTextBox($"已选择XCI文件: {selectedXciFilePath}");
+                }
+            }
+        }
+
+        private void SelectNcaFile()
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                dialog.Title = "选择NCA文件";
+                dialog.Filter = "NCA文件 (*.nca)|*.nca";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedNcaFilePath = dialog.FileName;
+                    UpdateRichTextBox($"已选择NCA文件: {selectedNcaFilePath}");
+                }
+            }
+        }
+
+        private void UnpackXCI()
+        {
+            if (!string.IsNullOrEmpty(selectedXciFilePath))
+            {
+                string encodedFilePath = Encoding.UTF8.GetString(Encoding.Default.GetBytes(selectedXciFilePath));
+                UpdateRichTextBox($"开始解包XCI文件: {encodedFilePath}");
+
+                new Thread(() =>
+                {
+                    string? directoryName = Path.GetDirectoryName(encodedFilePath);
+                    if (directoryName != null)
+                    {
+                        string outDir = Path.Combine(directoryName, "XCI_Extracted");
+                        Directory.CreateDirectory(outDir);
+                        RunProcess("hactool.exe", $"-t xci \"{encodedFilePath}\" --outdir \"{outDir}\"");
+                    }
+                    else
+                    {
+                        UpdateRichTextBox("无法获取文件路径的目录名。");
+                    }
+                }).Start();
+            }
+            else
+            {
+                UpdateRichTextBox("请先选择XCI文件再进行解包操作。");
+            }
+        }
+
+        private void UnpackNCA()
+        {
+            if (!string.IsNullOrEmpty(selectedNcaFilePath))
+            {
+                string encodedFilePath = Encoding.UTF8.GetString(Encoding.Default.GetBytes(selectedNcaFilePath));
+                UpdateRichTextBox($"开始解包NCA文件: {encodedFilePath}");
+
+                new Thread(() =>
+                {
+                    string? directoryName = Path.GetDirectoryName(encodedFilePath);
+                    if (directoryName != null)
+                    {
+                        string outDir = Path.Combine(directoryName, "NCA_Extracted");
+                        Directory.CreateDirectory(outDir);
+                        Directory.CreateDirectory(Path.Combine(outDir, "romfs"));
+                        Directory.CreateDirectory(Path.Combine(outDir, "exefs"));
+                        RunProcess("hactool.exe", $"-x -k keys.ini --romfsdir \"{outDir}/romfs\" --exefsdir \"{outDir}/exefs\" \"{encodedFilePath}\"");
+                    }
+                    else
+                    {
+                        UpdateRichTextBox("无法获取文件路径的目录名。");
+                    }
+                }).Start();
+            }
+            else
+            {
+                UpdateRichTextBox("请先选择NCA文件再进行解包操作。");
+            }
+        }
+
+        private void RunProcess(string exe, string args)
+        {
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = exe,
+                    Arguments = args,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                using (var process = new Process())
+                {
+                    process.StartInfo = psi;
+                    process.OutputDataReceived += (s, e) =>
+                    {
+                        if (!string.IsNullOrEmpty(e.Data))
+                        {
+                            UpdateRichTextBox(Encoding.UTF8.GetString(Encoding.Default.GetBytes(e.Data)));
+                        }
+                    };
+                    process.ErrorDataReceived += (s, e) =>
+                    {
+                        if (!string.IsNullOrEmpty(e.Data))
+                        {
+                            UpdateRichTextBox($"[错误] {Encoding.UTF8.GetString(Encoding.Default.GetBytes(e.Data))}");
+                        }
+                    };
+
+                    process.Start();
+                    process.BeginOutputReadLine();
+                    process.BeginErrorReadLine();
+                    process.WaitForExit();
+
+                    UpdateRichTextBox(process.ExitCode == 0
+                       ? "解包完成！"
+                        : $"解包失败，退出代码: {process.ExitCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                UpdateRichTextBox($"错误: {ex.Message}\n堆栈信息: {ex.StackTrace}");
+            }
+        }
+
+        private void UpdateRichTextBox(string message)
+        {
+            if (string.IsNullOrEmpty(message)) return;
+
+            if (rtbOutput.InvokeRequired)
+            {
+                rtbOutput.Invoke(new Action(() =>
+                {
+                    rtbOutput.AppendText($"{DateTime.Now:HH:mm:ss} - {message}\n");
+                    rtbOutput.ScrollToCaret();
+                }));
+            }
+            else
+            {
+                rtbOutput.AppendText($"{DateTime.Now:HH:mm:ss} - {message}\n");
+                rtbOutput.ScrollToCaret();
+            }
+        }
+
+        private void btnSelectXci_Click(object sender, EventArgs e) => SelectXciFile();
+        private void btnUnpackXCI_Click(object sender, EventArgs e) => UnpackXCI();
+        private void btnSelectNca_Click(object sender, EventArgs e) => SelectNcaFile();
+        private void btnUnpackNCA_Click(object sender, EventArgs e) => UnpackNCA();
+    }
+}
